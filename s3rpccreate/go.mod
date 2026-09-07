2b885f16da5730f313d3e4131c5663fcf5ac607b
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2026-09-07 18:29:26 +0200

    Update go version to 1.26

--- s3rpccreate/go.mod
+++ s3rpccreate/go.mod
@@ -1,6 +1,6 @@
 module github.com/bep/awscreate/s3rpccreate
 
-go 1.19
+go 1.26
 
 require (
 	github.com/aws/aws-sdk-go-v2 v1.16.14
---
+++ s3rpccreate/provisioner_test.go
@@ -0,0 +1,7 @@
+package s3rpccreate
+
+import "testing"
+
+func TestProvisioner(t *testing.T) {
+	// Empty for now, just to ensure the package compiles and the test framework is set up correctly.
+}
